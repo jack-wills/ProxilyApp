4d6ff6132d48e2e3ed71b0c8a3bb63eb6704f103
Jack Williams <jackw53519@gmail.com>
2019-03-16 10:35:03 +0000

    Better video control, pause when out of screen

--- components/VideoComponent.js
+++ components/VideoComponent.js
@@ -3,9 +3,14 @@ import { View, StyleSheet, Dimensions } from 'react-native'
 import Video from 'react-native-video'
 import {connect} from 'react-redux';
 import {bindActionCreators} from 'redux';
+
 import {updateVideoTime} from '../actions/UpdateVideo';
+import InView from './InView'
 
 class VideoComponent extends React.Component {
+  state = {
+    focused: false,
+  }
 
   componentDidMount() {
     this.props.navigation.addListener('willFocus', () => {
@@ -13,45 +18,60 @@ class VideoComponent extends React.Component {
         this.player.seek(this.props.currentVideoTime);
       }
     });
+    this.props.navigation.addListener('willBlur', () => {
+      if (this.player) {
+        this.player.seek(this.props.currentVideoTime);
+      }
+    });
+  }
+
+  checkVisible(isVisible) {
+    if (isVisible) {
+      this.setState({focused: true});
+    } else {
+      this.setState({focused: false});
+    }
   }
 
   renderVideo () {
       var videoWidth = Dimensions.get('window').width*0.96-2;
       return(
-        <Video
-          ref={plr => {
-            this.player = plr;
-          }}
-          source={{uri: this.props.url}}
-          style={{ 
-              width: videoWidth, 
-              height: videoWidth,
-              borderRadius: 30,
+          <Video
+            ref={plr => {
+              this.player = plr;
             }}
-          muted={false}
-          paused={!this.props.playing}
-          repeat={true}
-          resizeMode={"cover"}
-          volume={1.0}
-          rate={1.0}
-          ignoreSilentSwitch={"obey"}
-          onProgress={({currentTime}) => {
-            this.props.updateVideoTime(this.props.url, currentTime);
-          }}
-          onLoadStart={() => {
-            if (this.player) {
-              this.player.seek(this.props.currentVideoTime);
-            }
-          }}
+            source={{uri: this.props.url}}
+            style={{ 
+                width: videoWidth, 
+                height: videoWidth,
+                borderRadius: 30,
+              }}
+            muted={false}
+            paused={!this.props.playing || !this.state.focused}
+            repeat={true}
+            resizeMode={"cover"}
+            volume={1.0}
+            rate={1.0}
+            ignoreSilentSwitch={"obey"}
+            onProgress={({currentTime}) => {
+              this.props.updateVideoTime(this.props.url, currentTime);
+            }}
+            onLoadStart={() => {
+              if (this.player) {
+                this.player.seek(this.props.currentVideoTime);
+              }
+            }}
 
-        />
+          />
       )
   }
   render () {
     return (
-      <View style={styles.backgroundVideo}>
-        {this.renderVideo()}
-      </View>
+      <InView onChange={(isVisible) => this.checkVisible(isVisible)}>
+        <View style={styles.backgroundVideo}>
+          {this.renderVideo()}
+        </View>
+      </InView>
     )
   }
 }
